42164012aae60fc5bce7472eabda1e44fa4e8638
bunnysunny24 <bhavashesh@gmail.com>
2025-03-03 17:56:55 +0530

    woking on marketplace blockchain

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
-import StorageForm from "./components/StorageForm";
+ // import StorageForm from "./components/StorageForm";
+import Marketplace from "./components/Marketplace";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>🌾 Crop Storage Management</h1>
-        <StorageForm />
-        <hr />
+      <Marketplace />
       </header>
     </div>
   );
@@ -18,4 +17,8 @@ export default App;
 
 
 {/*<Marketplace />
-  import Marketplace from "./components/Marketplace";*/}
+  import Marketplace from "./components/Marketplace";
+  <h1>🌾 Crop Storage Management</h1>
+        <StorageForm />
+        <hr />*/
+}
