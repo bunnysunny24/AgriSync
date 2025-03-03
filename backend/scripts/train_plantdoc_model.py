b3fa5cbe56aa65df26c244dac58a7d2b8e98f59a
bunnysunny24 <bhavashesh@gmail.com>
2025-03-03 19:03:18 +0530

    increasing accuracy

--- backend/scripts/train_plantdoc_model.py
+++ backend/scripts/train_plantdoc_model.py
@@ -22,7 +22,7 @@ train_data = datagen.flow_from_directory(
 )
 
 val_data = ImageDataGenerator(rescale=1.0 / 255).flow_from_directory(
-    directory="PlantDoc-Dataset/val",
+    directory="PlantDoc-Dataset/test",
     target_size=(224, 224),
     batch_size=32,
     class_mode="categorical"
